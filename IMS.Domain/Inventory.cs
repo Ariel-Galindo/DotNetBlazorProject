c0baf76533c663cc471e220c7f53a05162a416b7
Ariel Galindo <arielgalindo77@gmail.com>
2026-03-30 12:22:43 -0700

    feat: adding properties to Inventory.cs

--- IMS.Domain/Inventory.cs
+++ IMS.Domain/Inventory.cs
@@ -1,8 +1,9 @@
-using System;
-
 namespace IMS.Domain;
 
 public class Inventory
 {
-
+    public int InventoryID { get; set; }
+    public string InventoryName { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public double Price { get; set; }
 }
