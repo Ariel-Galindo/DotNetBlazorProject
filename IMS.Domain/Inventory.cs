4543d39d5dc22179074a6c32f29c2f7463845ec9
Ariel Galindo <arielgalindo77@gmail.com>
2026-04-02 10:03:42 -0700

    fix: adding user validation in AddInventoryComponent

--- IMS.Domain/Inventory.cs
+++ IMS.Domain/Inventory.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace IMS.Domain;
 
 public class Inventory
 {
     public int InventoryID { get; set; }
+
+    [Required]
+    [StringLength(150)]
     public string InventoryName { get; set; } = string.Empty;
+
+    [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater or equal to 0.")]
     public int Quantity { get; set; }
+
+    [Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal to 0.")]
     public double Price { get; set; }
 }
