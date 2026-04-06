ffeb627039d8ac51c89bcab9ce54a8724e2855b7
Ariel Galindo <arielgalindo77@gmail.com>
2026-04-06 13:21:08 -0700

    feat: adding searchComponent

--- IMS.InMemory/InventoryRepository.cs
+++ IMS.InMemory/InventoryRepository.cs
@@ -10,10 +10,12 @@ public class InventoryRepository : IInventoryRepository
 
     public InventoryRepository()
     {
-        _inventories = [new Inventory { InventoryID = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2 }];
-        _inventories = [new Inventory { InventoryID = 2, InventoryName = "Bike Body", Quantity = 20, Price = 4 }];
-        _inventories = [new Inventory { InventoryID = 3, InventoryName = "Bike Wheels", Quantity = 30, Price = 6 }];
-        _inventories = [new Inventory { InventoryID = 4, InventoryName = "Bike Pedels", Quantity = 40, Price = 8 }];
+        _inventories = [
+            new Inventory { InventoryID = 1, InventoryName = "Bike Seat", Quantity = 10, Price = 2 },
+            new Inventory { InventoryID = 2, InventoryName = "Bike Body", Quantity = 20, Price = 4 },
+            new Inventory { InventoryID = 3, InventoryName = "Bike Wheels", Quantity = 30, Price = 6 },
+            new Inventory { InventoryID = 4, InventoryName = "Bike Pedels", Quantity = 40, Price = 8 }
+        ];
     }
 
     public Task AddInventoryAsync(Inventory inventory)
